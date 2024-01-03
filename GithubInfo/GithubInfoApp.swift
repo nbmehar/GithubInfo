e746cde173454887dc717083f18030a16d3b25cb
Nayab Mehar <nayab@grappus.com>
2024-01-03 16:05:02 +0530

    Initial Commit

---
+++ GithubInfo/GithubInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubInfoApp.swift
+//  GithubInfo
+//
+//  Created by Nayab Mehar on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
